0db9097df3302c6e9b7021079e4587575cc77ff0
Ulf Rueegg <uru@informatik.uni-kiel.de>
2014-12-01 14:46:07 +0100

    removed ecoretools dependency

--- plugins/de.cau.cs.kieler.ecoreviz/src/de/cau/cs/kieler/ecoreviz/OpenDiagramHandler.java
+++ plugins/de.cau.cs.kieler.ecoreviz/src/de/cau/cs/kieler/ecoreviz/OpenDiagramHandler.java
@@ -13,26 +13,30 @@
  */
 package de.cau.cs.kieler.ecoreviz;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.core.internal.resources.File;
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EModelElement;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
-import org.eclipse.emf.ecoretools.diagram.navigator.EcoreDomainNavigatorItem;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.eclipse.ui.statushandlers.StatusManager;
+
+import com.google.common.base.Predicate;
+import com.google.common.collect.Iterators;
 
 import de.cau.cs.kieler.klighd.KlighdTreeSelection;
 import de.cau.cs.kieler.klighd.ui.DiagramViewManager;
@@ -43,14 +47,34 @@ import de.cau.cs.kieler.klighd.util.KlighdSynthesisProperties;
  * 
  * @author ckru
  */
-@SuppressWarnings("restriction")
 public class OpenDiagramHandler extends AbstractHandler {
 
     public static final String PLUGIN_ID = "de.cau.cs.kieler.ecoreviz";
 
     /**
+     * Tests if all elements of an iterator are {@link EClass} or
+     * {@link EPackage}.
+     */
+    private static final Predicate<Object> EPACKAGE_ECLASS_PREDICATE = new Predicate<Object>() {
+        public boolean apply(final Object o) {
+            return o instanceof EPackage || o instanceof EClass;
+        }
+    };
+    
+    /**
+     * Tests if all elements of an iterator are either assignment-compatible with {@link EClass} or
+     * {@link EPackage}.
+     */
+    private static final Predicate<Object> EMODEL_ELEMENT_PREDICATE = new Predicate<Object>() {
+        public boolean apply(final Object o) {
+            return o instanceof EModelElement;
+        }
+    };
+    
+    /**
      * {@inheritDoc}
      */
+    @SuppressWarnings("unchecked")
     public Object execute(final ExecutionEvent event) throws ExecutionException {
         final ISelection selection = HandlerUtil.getCurrentSelection(event);
 
@@ -61,33 +85,39 @@ public class OpenDiagramHandler extends AbstractHandler {
                 // do not react on selections in KLighD diagrams
                 return null;
             }
-
-            for (Object o : sSelection.toArray()) {
-                if (o instanceof EcoreDomainNavigatorItem
-                        && ((EcoreDomainNavigatorItem) o).getEObject() instanceof EModelElement) {
-                    listSelection.add((EModelElement) ((EcoreDomainNavigatorItem) o).getEObject());
-                } else if (o instanceof EModelElement) {
-                    listSelection.add((EModelElement) o);
-                } else if (o instanceof File) {
-                    try {
-                        File f = (File) o;
-                        ResourceSet rs = new ResourceSetImpl();
-                        Resource r =
-                                rs.getResource(URI.createFileURI(f.getFullPath().toString()), true);
-                        if (r.getContents().size() > 0) {
-                            if (r.getContents().get(0) instanceof EPackage) {
-                                listSelection.add((EModelElement) r.getContents().get(0));
+            
+            // if all elements are either ecore packages or classes
+            if (Iterators.all(sSelection.iterator(), EPACKAGE_ECLASS_PREDICATE)) {
+                Iterator<EModelElement> emIt =
+                        Iterators.filter(sSelection.iterator(), EMODEL_ELEMENT_PREDICATE);
+                while (emIt.hasNext()) {
+                    listSelection.add(emIt.next());
+                }
+            } else {
+                // otherwise check the elements piece by piece for files
+                for (Object o : sSelection.toArray()) {
+                    if (o instanceof IFile) {
+                        try {
+                            IFile f = (IFile) o;
+                            ResourceSet rs = new ResourceSetImpl();
+                            Resource r = rs.getResource(URI.createFileURI(f.getFullPath().toString()),
+                                            true);
+                            if (r.getContents().size() > 0) {
+                                if (r.getContents().get(0) instanceof EPackage) {
+                                    listSelection.add((EModelElement) r.getContents().get(0));
+                                }
                             }
+                        } catch (Exception e) {
+                            StatusManager
+                                    .getManager()
+                                    .handle(new Status(IStatus.ERROR, PLUGIN_ID,
+                                            "Could not load selected file.", e), StatusManager.SHOW);
                         }
-                    } catch (Exception e) {
-                        StatusManager.getManager().handle(
-                                new Status(IStatus.ERROR, PLUGIN_ID,
-                                        "Could not load selected file.", e), StatusManager.SHOW);
-                    }
-                } else {
-                    handleUnknownSelection(selection);
+                    } else {
+                        handleUnknownSelection(selection);
 
-                    return null;
+                        return null;
+                    }
                 }
             }
 
